7ccea7f7e4df06a26a2e01ee192f7037d2287a60
Diego Berrocal <cestdiego@gmail.com>
2015-01-29 02:30:53 -0500

    document ready

--- reto.js
+++ reto.js
@@ -1,61 +1,63 @@
-var ref = new Firebase("https://reto-3-backend.firebaseio.com");
-ref.onAuth(function(authData) {
-    if (authData) {
-        // user authenticated with Firebase
-        document.querySelector('.overlay').classList.add('dontDisplay');
-        document.querySelector('.wrapper').classList.remove("blur");
-        document.querySelector('.header').classList.remove("blur");
+$(document).ready(function(){
+    var ref = new Firebase("https://reto-3-backend.firebaseio.com");
+    ref.onAuth(function(authData) {
+        if (authData) {
+            // user authenticated with Firebase
+            document.querySelector('.overlay').classList.add('dontDisplay');
+            document.querySelector('.wrapper').classList.remove("blur");
+            document.querySelector('.header').classList.remove("blur");
 
 
-        howToGetProfilePic = findProfilePic(authData);
-        howToGetFullName = findFullName(authData);
+            howToGetProfilePic = findProfilePic(authData);
+            howToGetFullName = findFullName(authData);
 
-        document.querySelector('.logout').addEventListener('click', function(event){
-            ref.unauth();  
-        });
+            document.querySelector('.logout').addEventListener('click', function(event){
+                ref.unauth();  
+            });
 
-        // save the user 
-        ref.child('users').child(authData.uid).set(authData);
-    } else {
-        // user is logged out
-        document.querySelector('.overlay').classList.remove('dontDisplay');
-        document.querySelector('.wrapper').classList.add("blur");
-        document.querySelector('.header').classList.add("blur");
-    }
-});
-
-document.querySelector('.fa-facebook-square').addEventListener('click', function(){
-    userLogin("facebook");
-});
-// document.querySelector('.fa-twitter-square').addEventListener('click', function(){
-//     userLogin("twitter");
-// });
-
-function userLogin(Provider){
-    ref.authWithOAuthRedirect(Provider, function(err, authData){
-        console.log(authData);
+            // save the user 
+            ref.child('users').child(authData.uid).set(authData);
+        } else {
+            // user is logged out
+            document.querySelector('.overlay').classList.remove('dontDisplay');
+            document.querySelector('.wrapper').classList.add("blur");
+            document.querySelector('.header').classList.add("blur");
+        }
     });
-}
-function findProfilePic(authData){
-    var provider = authData.provider;
-    if (provider == "facebook"){
-        return "http://graph.facebook.com/" +
-            authData.uid.split(":")[1] +
-            "/picture?width=40&height=40";
+
+    document.querySelector('.fa-facebook-square').addEventListener('click', function(){
+        userLogin("facebook");
+    });
+    // document.querySelector('.fa-twitter-square').addEventListener('click', function(){
+    //     userLogin("twitter");
+    // });
+
+    function userLogin(Provider){
+        ref.authWithOAuthRedirect(Provider, function(err, authData){
+            console.log(authData);
+        });
     }
-    else if (provider == "twitter"){
-        return "http://avatars.io/twitter/"+authData.twitter.username +"?size=large";
+    function findProfilePic(authData){
+        var provider = authData.provider;
+        if (provider == "facebook"){
+            return "http://graph.facebook.com/" +
+                authData.uid.split(":")[1] +
+                "/picture?width=40&height=40";
+        }
+        else if (provider == "twitter"){
+            return "http://avatars.io/twitter/"+authData.twitter.username +"?size=large";
+        };
+        
+        return "Not found";
     };
-    
-    return "Not found";
-};
-function findFullName(authData){
-    var provider = authData.provider;
-    if (provider == "facebook"){
-        return authData.facebook.displayName;
+    function findFullName(authData){
+        var provider = authData.provider;
+        if (provider == "facebook"){
+            return authData.facebook.displayName;
+        }
+        else if (provider == "twitter"){
+            return authData.twitter.displayName; 
+        }
+        return "Anonymous Frog";
     }
-    else if (provider == "twitter"){
-        return authData.twitter.displayName; 
-    }
-    return "Anonymous Frog";
-}
+});
